7a2793c95224c35bd1f4d143908c0595646885a1
XuPengfei <xupengfei@thtf.com.cn>
2020-08-01 15:55:03 +0800

    提交项目

---
+++ GLSLTriangleDemo/GLSLTriangleDemo/AppDelegate.m
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.m
+//  GLSLTriangleDemo
+//
+//  Created by 徐鹏飞 on 2020/8/1.
+//  Copyright © 2020 徐鹏飞. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@interface AppDelegate ()
+
+@end
+
+@implementation AppDelegate
+
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    // Override point for customization after application launch.
+    return YES;
+}
+
+
+
+
+
+@end
---
+++ GLSLTriangleDemo/GLSLTriangleDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GLSLTriangleDemo
+//
+//  Created by 徐鹏飞 on 2020/8/1.
+//  Copyright © 2020 徐鹏飞. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
